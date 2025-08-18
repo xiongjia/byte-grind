f095bd9eeda314a26821d0183428f535711afa96
xiongjia <lexiongjia@gmail.com>
2025-08-18 22:19:59 +0800

    api server

--- oat/oat-api/src/main/java/bytegrind/oat/api/HelloController.java
+++ oat/oat-api/src/main/java/bytegrind/oat/api/HelloController.java
@@ -3,12 +3,14 @@ package bytegrind.oat.api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import bytegrind.oat.infra.Test;
 
 @RestController
 public class HelloController {
   // http://localhost:8082/api/hello?name=World
   @GetMapping("/api/hello")
   public String sayHello(@RequestParam(defaultValue = "Guest") String name) {
-    return "Hello, " + name + "!";
+    final Test t = new Test();
+    return "Hello, " + t.test() + ", "+ name + "!";
   }
 }
