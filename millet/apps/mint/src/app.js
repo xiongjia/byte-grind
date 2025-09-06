222af755478ea86c97fa99381d72cfe9ff5102c7
xiongjia <lexiongjia@gmail.com>
2025-09-06 15:37:40 +0800

    adding storybook

---
+++ millet/apps/mint/src/app.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import { Button } from '@millet/component';
+export default function App() {
+    return (_jsxs("div", { style: { padding: 20 }, children: [_jsx("h1", { children: "Hello from Web App" }), _jsx(Button, { children: "Click Me" })] }));
+}
---
+++ millet/apps/mint/src/main.js
@@ -0,0 +1,9 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './app';
+const root = document.getElementById('root');
+if (root) {
+    createRoot(root).render(_jsx(StrictMode, { children: _jsx(App, {}) }));
+}
