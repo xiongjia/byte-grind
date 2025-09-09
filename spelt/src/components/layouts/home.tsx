69807259e66d26f42349cca9144e7256a2356843
xiongjia <lexiongjia@gmail.com>
2025-09-09 23:45:28 +0800

    updating cards

--- spelt/src/components/layouts/home.tsx
+++ spelt/src/components/layouts/home.tsx
@@ -1,26 +1,43 @@
-import { cx } from 'class-variance-authority'
-import { Button, buttonVariants } from '@/components/ui/button'
+import { Overview } from '@/components/layouts/overview'
+import { Main } from '@/components/layouts/main'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { useTheme } from '@/context/theme-provider'
+import { Button } from '@/components/ui/button'
 
 const Home = () => {
   const { theme, setTheme } = useTheme()
+
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button
-        className={cx(buttonVariants({ variant: 'default', size: 'sm' }))}
-        onClick={() => {
-          console.log('toggle theme', theme)
-          if (theme === 'system') {
-            setTheme('dark')
-            return
-          }
-          setTheme(theme === 'light' ? 'dark' : 'light')
-          console.log('new theme', theme)
-        }}
-      >
-        Click me
-      </Button>
-    </div>
+    <Main>
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-7">
+        <Card className="col-span-1 lg:col-span-4">
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent className="ps-2">
+            <Overview />
+          </CardContent>
+        </Card>
+      </div>
+
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button
+          variant={'default'}
+          size={'sm'}
+          onClick={() => {
+            console.log('toggle theme', theme)
+            if (theme === 'system') {
+              setTheme('dark')
+              return
+            }
+            setTheme(theme === 'light' ? 'dark' : 'light')
+            console.log('new theme', theme)
+          }}
+        >
+          Click me
+        </Button>
+      </div>
+    </Main>
   )
 }
 
