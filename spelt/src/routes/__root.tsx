c627c3925162d79b48bde6e82303188e179c976c
xiongjia <lexiongjia@gmail.com>
2025-09-09 23:24:50 +0800

    update menu bar

--- spelt/src/routes/__root.tsx
+++ spelt/src/routes/__root.tsx
@@ -1,19 +1,33 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { AppSidebar } from '@/components/layouts/app-sidebar'
+import { Header } from '@/components/layouts/header'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import { cn } from '@/lib/utils'
 
 const RootLayout = () => (
   <>
-    {/* <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{' '}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>
-    </div>
-    <hr /> */}
-    <Outlet />
+    <SidebarProvider defaultOpen={true}>
+      <AppSidebar />
+      <SidebarInset
+        className={cn(
+          // Set content container, so we can use container queries
+          '@container/content',
+
+          // If layout is fixed, set the height
+          // to 100svh to prevent overflow
+          'has-[[data-layout=fixed]]:h-svh',
+
+          // If layout is fixed and sidebar is inset,
+          // set the height to 100svh - spacing (total margins) to prevent overflow
+          'peer-data-[variant=inset]:has-[[data-layout=fixed]]:h-[calc(100svh-(var(--spacing)*4))]',
+        )}
+      >
+        <Header>Test2</Header>
+        <Outlet />
+      </SidebarInset>
+    </SidebarProvider>
     {import.meta.env.MODE === 'development' && (
       <>
         <TanStackRouterDevtools position="bottom-right" />
