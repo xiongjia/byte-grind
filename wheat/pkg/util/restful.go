69bd45eb492acd0408ecb6ac4f03da2bb939a653
xiongjia <lexiongjia@gmail.com>
2025-09-12 16:56:52 +0800

    updating gin tests

--- wheat/pkg/util/restful.go
+++ wheat/pkg/util/restful.go
@@ -1,14 +1,27 @@
 package util
 
 import (
+	"log/slog"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+func customMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+		c.Next()
+		latency := time.Since(t)
+		reqUrl := c.Request.RequestURI
+		slog.Debug("custom middleware", slog.Duration("latancy", latency), slog.String("req", reqUrl))
+	}
+}
+
 func NewGinRouter() (http.Handler, error) {
 	router := gin.New()
 	router.Use(gin.Recovery())
+	router.Use(customMiddleware())
 	router.GET("/hello", func(c *gin.Context) {
 		c.String(http.StatusOK, "hello")
 	})
